01eb618a4fed007e679e53a7d3b76052efcd7ce8
Charlotte Pages <pgs.charlotte@gmail.com>
2018-08-17 11:58:53 +0200

    removed link to images

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
   resources :cocktails, only: %i[index new create show] do
     resources :doses, only: %i[new create destroy], shallow: true
   end
