8c2be82daf992d19e865b291b5cf56eff87e9745
Charlotte Pages <pgs.charlotte@gmail.com>
2018-08-17 17:05:16 +0200

    small fix on search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,15 +3,11 @@ class SearchController < ApplicationController
   before_action :set_tags, only: %i[index]
 
   def index
-
-  end
-
-  private
-  def set_tags
+    @cocktail_name = @cocktail.name
     @ingredients = @cocktail.ingredients
   end
 
-  def
+  private
 
   def set_cocktail
     @cocktail = Cocktail.find(params[:cocktail_id])
